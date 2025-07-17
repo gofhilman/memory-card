33e6c54fd6d71ba6e2bc353eba389301df144277
Hilman Fikry <hilman12fikry@gmail.com>
2025-07-17 23:35:20 +0700

    WIP: create Game component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import pokemonData from "../data/pokemon-data";
+
+const REVEAL_NUMBER = 12;
 
 export default function Game({ setScoreboard }) {
   const [catchedPokemon, setCatchedPokemon] = useState([]);
-  return (
-    <div>
-      
-    </div>
-  )
+
+  return <div></div>;
+}
+
+function revealPokemon(data, catchedData) {
+  // At least one pokemon hasn't been catched yet
+  let unpickedId = randomizeInt(data.length);
+  while (catchedData.some((item) => item.id === unpickedId)) {
+    unpickedId = randomizeInt(data.length);
+  }
+  const revealedIds = [unpickedId];
+  // The rest is not cared about as long as no duplication exists
+  for (let iter = 0; iter < REVEAL_NUMBER - 1; iter++) {
+    let randomId = randomizeInt(data.length);
+    while (revealedIds.includes(randomId)) {
+      randomId = randomizeInt(data.length);
+    }
+    revealedIds.push(randomId);
+  }
+  return revealedIds.map((id) => {
+    return data.find((item) => item.id === id);
+  });
+}
+
+function randomizeInt(number) {
+  return Math.floor(Math.random() * number);
 }
--- src/data/pokemon-data.js
+++ src/data/pokemon-data.js
@@ -1,10 +1,20 @@
+// HOW TO USE
+// const jsonObj = {results: [{ id: pokemonId, name: pokemonName, url: pokemonUrl }, ...]};
+
 const CARD_VARIATION = 100;
 const pokemonListUrl = `https://pokeapi.co/api/v2/pokemon?limit=${CARD_VARIATION}&offset=0`;
 
-const pokemonData = [];
+async function fetchPokemonList(listUrl) {
+  const response = await fetch(listUrl, { mode: "cors" });
+  if (!response.ok) throw new Error(`Response status: ${response.status}`);
+  const data = await response.json();
+  return data.results.map((item, index) => {
+    item.id = index;
+    return item;
+  });
+}
 
-// HOW TO USE
+// fetchPokemonList(pokemonListUrl).then((data) => console.log(data));
 
-// const obj = {results: {id: {name: pokemonName, url: pokemonUrl}}};
+export default fetchPokemonList(pokemonListUrl);
 
-export default pokemonData;
